5a969ee3e10c709cd0a84542bfe6c7490231e05b
Kuba <ja.tanski@gmail.com>
2019-05-08 12:47:25 +0200

    fix little bug

--- dist/index.js
+++ dist/index.js
@@ -144,132 +144,132 @@ let weekDayIterator = 0
 let totalTime = 0
 
 const calcInvestEUR = () => {
-   const investAmountPLN = inputInvestAmount.value
-   reinvestTime = inputInvestTime.value
-   euroPrice = inputEuroPrice.value
-   startInvestBox.investAmount = investAmountPLN / euroPrice
-   startInvestBox.investAmount = Math.round(startInvestBox.investAmount * 100) / 100
+    const investAmountPLN = inputInvestAmount.value
+    reinvestTime = inputInvestTime.value
+    euroPrice = inputEuroPrice.value
+    startInvestBox.investAmount = investAmountPLN / euroPrice
+    startInvestBox.investAmount = Math.round(startInvestBox.investAmount * 100) / 100
 }
 
 const calcEndInvestDay = () => {
-   const endTime = document.querySelector('.result-end-time')
-   const date = new Date()
-   const now = date.getTime()
-   const endInvestMinsec = now + totalTime * 24 * 60 * 60 * 1000
-   const endInvestDay = new Date(endInvestMinsec)
-   endTime.innerText = `Dzień wypłaty środków: ${endInvestDay.getDate()}.${endInvestDay.getMonth() + 1}.${endInvestDay.getFullYear()}r.`
+    const endTime = document.querySelector('.result-end-time')
+    const date = new Date()
+    const now = date.getTime()
+    const endInvestMinsec = now + totalTime * 24 * 60 * 60 * 1000
+    const endInvestDay = new Date(endInvestMinsec)
+    endTime.innerText = `Dzień wypłaty środków: ${endInvestDay.getDate()}.${endInvestDay.getMonth() + 1}.${endInvestDay.getFullYear()}r.`
 }
 
 const calcDailyIncome = () => {
-   calcInvestEUR()
-   if (!inputSix.checked && !inputTwelve.checked) alert('Wybierz plan inwestycyjny.')
-   if (inputSix.checked == true) {
-      multiplier = 1.3;
-      boxTime = 182
-   } else if (inputTwelve.checked == true) {
-      multiplier = 1.7;
-      boxTime = 365
-   }
+    calcInvestEUR()
+    if (!inputSix.checked && !inputTwelve.checked) alert('Wybierz plan inwestycyjny.')
+    if (inputSix.checked == true) {
+        multiplier = 1.3;
+        boxTime = 182
+    } else if (inputTwelve.checked == true) {
+        multiplier = 1.7;
+        boxTime = 365
+    }
 }
 
 const calcTotalTime = () => {
-   if (!reinvestTime || parseInt(reinvestTime) + timeToEnd < boxTime && !reInvestBoxArray[reInvestBoxArray.length - 1]) totalTime = boxTime
-   else if (parseInt(reinvestTime) + timeToEnd < boxTime && reInvestBoxArray[reInvestBoxArray.length - 1]) totalTime = boxTime + reInvestBoxArray[reInvestBoxArray.length - 1].passedTime
-   else totalTime = parseInt(reinvestTime) + timeToEnd
+    if (!reinvestTime || parseInt(reinvestTime) + timeToEnd < boxTime && !reInvestBoxArray[reInvestBoxArray.length - 1]) totalTime = boxTime
+    else if (parseInt(reinvestTime) + timeToEnd < boxTime && reInvestBoxArray[reInvestBoxArray.length - 1]) totalTime = boxTime + reInvestBoxArray[reInvestBoxArray.length - 1].passedTime
+    else totalTime = parseInt(reinvestTime) + timeToEnd
 }
 
 const showResult = () => {
-   const showTime = document.querySelector('.result-total-time')
-   const showIncome = document.querySelector('.result-total-income')
-   const showRoi = document.querySelector('.result-roi')
+    const showTime = document.querySelector('.result-total-time')
+    const showIncome = document.querySelector('.result-total-income')
+    const showRoi = document.querySelector('.result-roi')
 
-   calcTotalTime()
-   calcEndInvestDay()
-   const totalIncome = Math.round(balance * euroPrice)
-   const roi = Math.round((totalIncome / inputInvestAmount.value) * 100)
+    calcTotalTime()
+    calcEndInvestDay()
+    const totalIncome = Math.round(balance * euroPrice)
+    const roi = Math.round((totalIncome / inputInvestAmount.value) * 100)
 
-   showTime.innerText = `Ilośc dni od rozpoczęcią inwestycji: ${totalTime}.`
-   showIncome.innerText = `Stan konta na ostatni dzień: ${totalIncome} PLN.`
-   showRoi.innerText = `Twój zysk wyniesie ${roi}%`
+    showTime.innerText = `Ilośc dni od rozpoczęcią inwestycji: ${totalTime}.`
+    showIncome.innerText = `Stan konta na ostatni dzień: ${totalIncome} PLN.`
+    showRoi.innerText = `Twój zysk wyniesie ${roi}%`
 }
 
 const calcBalance = (e) => {
-   e.preventDefault()
-   calcDailyIncome()
-   let dailyIncomeReinvestBox = []
-   let time = 0
-   const timeCalc = () => {
-      if (!reinvestTime || reinvestTime < boxTime) time = boxTime
-      else time = reinvestTime
-   }
-   timeCalc()
-   for (let i = 0; i < time; i++) {
-      if (!reInvestBoxArray[0]) dailyIncome = startInvestBox.investAmount * multiplier / boxTime
-      else {
-         for (let i = 0; i < reInvestBoxArray.length; i++) {
-            dailyIncomeReinvestBox[i] = reInvestBoxArray[i].investAmount * multiplier / boxTime
-         }
-         let dailyIncomeReinvestSum = 0
-         dailyIncomeReinvestBox.forEach(el => {
+    e.preventDefault()
+    calcDailyIncome()
+    let dailyIncomeReinvestBox = []
+    let time = 0
+    const timeCalc = () => {
+        if (!reinvestTime || reinvestTime < boxTime) time = boxTime
+        else time = reinvestTime
+    }
+    timeCalc()
+    for (let i = 0; i < time; i++) {
+        if (!reInvestBoxArray[0]) dailyIncome = startInvestBox.investAmount * multiplier / boxTime
+        else {
+            for (let i = 0; i < reInvestBoxArray.length; i++) {
+                dailyIncomeReinvestBox[i] = reInvestBoxArray[i].investAmount * multiplier / boxTime
+            }
+            let dailyIncomeReinvestSum = 0
+            dailyIncomeReinvestBox.forEach(el => {
+                dailyIncomeReinvestSum += el
+            });
+
+            dailyIncome = startInvestBox.investAmount * multiplier / boxTime + dailyIncomeReinvestSum
+        }
+        balance += dailyIncome
+        if (balance >= 50 && weekDayIterator == 0 && inputYes.checked && i < reinvestTime) {
+            reInvestBoxArray[reInvestBoxArray.length] = new _Invest__WEBPACK_IMPORTED_MODULE_0__["Invest"](balance, 0)
+            balance = 0
+            weekDayIterator = 7
+        }
+        startInvestBox.passedTime++
+        if (weekDayIterator != 0) weekDayIterator--
+        if (startInvestBox.passedTime >= boxTime) startInvestBox.investAmount = 0
+
+        reInvestBoxArray.forEach(el => {
+            el.passedTime++
+        })
+        reInvestBoxArray.forEach(el => {
+            if (el.passedTime >= boxTime) el.investAmount = 0
+        })
+    }
+    startInvestBox = null
+    console.log(dailyIncomeReinvestBox)
+    console.log(reInvestBoxArray, startInvestBox, balance, multiplier, boxTime, reinvestTime)
+    if (inputYes.checked && reInvestBoxArray[reInvestBoxArray.length - 1]) timeToEnd = boxTime - reInvestBoxArray[reInvestBoxArray.length - 1].passedTime
+    for (let i = 0; i < timeToEnd; i++) {
+        let dailyIncomeReinvestSum = 0
+        dailyIncomeReinvestBox.forEach(el => {
             dailyIncomeReinvestSum += el
-         });
-
-         dailyIncome = startInvestBox.investAmount * multiplier / boxTime + dailyIncomeReinvestSum
-      }
-      balance += dailyIncome
-      if (balance >= 50 && weekDayIterator == 0 && inputYes.checked && i < reinvestTime) {
-         reInvestBoxArray[reInvestBoxArray.length] = new _Invest__WEBPACK_IMPORTED_MODULE_0__["Invest"](balance, 0)
-         balance = 0
-         weekDayIterator = 7
-      }
-      startInvestBox.passedTime++
-      if (weekDayIterator != 0) weekDayIterator--
-      if (startInvestBox.passedTime >= boxTime) startInvestBox.investAmount = 0
-
-      reInvestBoxArray.forEach(el => {
-         el.passedTime++
-      })
-      reInvestBoxArray.forEach(el => {
-         if (el.passedTime >= boxTime) el.investAmount = 0
-      })
-   }
-   startInvestBox = null
-   console.log(dailyIncomeReinvestBox)
-   console.log(reInvestBoxArray, startInvestBox, balance, multiplier, boxTime, reinvestTime)
-   if (inputYes.checked && reInvestBoxArray[reInvestBoxArray.length - 1]) timeToEnd = boxTime - reInvestBoxArray[reInvestBoxArray.length - 1].passedTime
-   for (let i = 0; i < timeToEnd; i++) {
-      let dailyIncomeReinvestSum = 0
-      dailyIncomeReinvestBox.forEach(el => {
-         dailyIncomeReinvestSum += el
-      });
-      balance += dailyIncomeReinvestSum
-      reInvestBoxArray.forEach(el => {
-         el.passedTime++
-      })
-      for (let i = 0; i < reInvestBoxArray.length; i++) {
-         const el = reInvestBoxArray[i]
-         if (el.passedTime >= boxTime) {
-            el.investAmount = 0
-            dailyIncomeReinvestBox[i] = 0
-         }
-      }
-   }
-   showResult()
+        });
+        balance += dailyIncomeReinvestSum
+        reInvestBoxArray.forEach(el => {
+            el.passedTime++
+        })
+        for (let i = 0; i < reInvestBoxArray.length; i++) {
+            const el = reInvestBoxArray[i]
+            if (el.passedTime >= boxTime) {
+                el.investAmount = 0
+                dailyIncomeReinvestBox[i] = 0
+            }
+        }
+    }
+    showResult()
 }
 
 function reinvestDisabled() {
-   if (this.checked) {
-      inputInvestTime.setAttribute('disabled', 'disabled')
-      inputInvestTime.value = ''
-      investTimeSection.style.display = 'none'
-   }
+    if (this.checked) {
+        inputInvestTime.setAttribute('disabled', 'disabled')
+        inputInvestTime.value = ''
+        investTimeSection.style.display = 'none'
+    }
 }
 
 function reinvestDisabledOut() {
-   if (this.checked) {
-      inputInvestTime.removeAttribute('disabled')
-      investTimeSection.style.display = 'block'
-   }
+    if (this.checked) {
+        inputInvestTime.removeAttribute('disabled')
+        investTimeSection.style.display = 'block'
+    }
 }
 
 startButton.addEventListener('click', calcBalance)
@@ -277,12 +277,14 @@ inputNo.addEventListener('change', reinvestDisabled)
 inputYes.addEventListener('change', reinvestDisabledOut)
 _password__WEBPACK_IMPORTED_MODULE_1__["passwordButton"].addEventListener('click', _password__WEBPACK_IMPORTED_MODULE_1__["checkPassword"])
 document.addEventListener("DOMContentLoaded", function () {
-   const password = JSON.parse(localStorage.getItem('passwordValue'))
-   if (password.value == "Dupa1") _password__WEBPACK_IMPORTED_MODULE_1__["passwordSection"].style.display = 'none'
+    const password = JSON.parse(localStorage.getItem('passwordValue'));
+    if (password.value) {
+        if (password.value == "Ekipa1") _password__WEBPACK_IMPORTED_MODULE_1__["passwordSection"].style.display = 'none'
+    }
 });
 
 document.querySelector('.changes-form--button-reset').addEventListener('click', function () {
-   location.reload()
+    location.reload()
 })
 
 /***/ }),
@@ -310,7 +312,7 @@ const checkPassword = (e) => {
         value: passwordInput.value
     }
 
-    if (password.value == 'Dupa1') {
+    if (password.value == 'Ekipa1') {
         passwordSection.style.display = 'none'
         localStorage.setItem('passwordValue', JSON.stringify(password))
     } else {
